0ba26d87d5a6eb2e9194324e502d5307cb252bd0
Patrick Wu <scorpio7500@gmail.com>
2014-05-27 04:13:44 -0400

    changed README and closestEnemy

--- greed.js
+++ greed.js
@@ -15,29 +15,30 @@ var enemies = base.getEnemies();
 //greedy appraoch
 //How do I stop them from changing direction halfway through if this method runs every frame, cannot save global variables
 for (var peonIndex = 0; peonIndex < peons.length; peonIndex++) {
-	    var peon = peons[peonIndex];
-		    var coin = peon.getNearest(coins);
-			    base.command(peon, 'move', coin.pos);
+    var peon = peons[peonIndex];
+    var coin = peon.getNearest(coins);
+    base.command(peon, 'move', coin.pos);
 }
 /* original code 
 
-   for (var peonIndex = 0; peonIndex < peons.length; peonIndex++) {
-       var peon = peons[peonIndex];
-	       var item = base.getNearest(items);
-		       if (item)
-			           base.command(peon, 'move', item.pos);
-					   } */
+for (var peonIndex = 0; peonIndex < peons.length; peonIndex++) {
+    var peon = peons[peonIndex];
+    var item = base.getNearest(items);
+    if (item)
+        base.command(peon, 'move', item.pos);
+} */
 
 //find the enemy closest to the base
 var closestEnemy;
 for (var enemyIndex = 0; enemyIndex < enemies.length; enemyIndex++) {
-	    if ((closestEnemy === undefined) && (enemies[enemyIndex].type != 'peon'))
-			        closestEnemy = enemies[enemyIndex];
-		    else if (enemies[enemyIndex].type == 'peon') {
-				        continue;
-						    }
-			    else if (base.distance(closestEnemy) > base.distance(enemies[enemyIndex]))
-					        closestEnemy = enemies[enemyIndex];
+    if (enemies[enemyIndex].type == 'peon')
+        continue;
+        
+    if (closestEnemy === undefined)
+        closestEnemy = enemies[enemyIndex];
+
+    else if (base.distance(closestEnemy) > base.distance(enemies[enemyIndex]))
+        closestEnemy = enemies[enemyIndex];
 }
 
 /////// 2. Decide which unit to build this frame. ///////
@@ -46,25 +47,26 @@ for (var enemyIndex = 0; enemyIndex < enemies.length; enemyIndex++) {
 
 var type;
 
-if ((base.built.length === 0) || (peons.length < 4))
-	    type = 'peon';
-		else if (munchkins.length < 10)
-	    type = 'munchkin';
-		else if (ogres.length < 2)
-	    type = 'ogre';
-		else if ((ogres.length >= 2) || (munchkins.length >= 10)) 
-	    type = 'shaman';
-		else
-		    type = 'berserker';
-			    
-			if (base.gold >= base.buildables[type].goldCost)
-	    base.build(type);
+//makes peons unless enemies get close
+if ((base.built.length === 0) || (peons.length < 4) && (base.distance(closestEnemy) > 10))
+    type = 'peon';
+else if (munchkins.length < 10)
+    type = 'munchkin';
+else if (ogres.length < 2)
+    type = 'ogre';
+else if ((ogres.length >= 2) || (munchkins.length >= 10)) 
+    type = 'shaman';
+else
+    type = 'berserker';
+    
+if (base.gold >= base.buildables[type].goldCost)
+    base.build(type);
 
 
-		// 'peon': Peons gather gold and do not fight.
-		// 'munchkin': Light melee unit.
-		// 'ogre': Heavy melee unit.
-		// 'shaman': Support spellcaster.
-		// 'fangrider': High damage ranged attacker.
-		// 'brawler': Mythically expensive super melee unit.
-		// See the buildables documentation below for costs and the guide for more info.
+// 'peon': Peons gather gold and do not fight.
+// 'munchkin': Light melee unit.
+// 'ogre': Heavy melee unit.
+// 'shaman': Support spellcaster.
+// 'fangrider': High damage ranged attacker.
+// 'brawler': Mythically expensive super melee unit.
+// See the buildables documentation below for costs and the guide for more info.
